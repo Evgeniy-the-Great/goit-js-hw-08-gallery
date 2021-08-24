f7f02c03beb1cbbf3d7204cd9ee86b288646da00
Evgeniy-the-Great <shalenkoevgeniy@gmail.com>
2021-08-24 14:04:21 +0300

    add render of html set

--- src/app.js
+++ src/app.js
@@ -63,3 +63,31 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const ulRef = document.querySelector('.js-gallery');
+console.log(ulRef);
+const cartMarkup = createPictureCartMarkup(galleryItems);
+
+ulRef.insertAdjacentHTML('beforeend', cartMarkup);
+
+function createPictureCartMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+      <a
+        class="gallery__link"
+        href="${original}"
+      >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+    `;
+    })
+    .join('');
+}
